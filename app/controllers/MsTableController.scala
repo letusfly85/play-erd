d0f5b97545401c0c80ba5ab71d0d606ff3d7a5f2
letusfly85 <letusfly95@gmail.com>
2013-09-15 16:34:30 +0900

    change controller from Application to MsTableController

--- app/controllers/MsTableController.scala
+++ app/controllers/MsTableController.scala
@@ -1,27 +1,28 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import models.MsTable
+import play.api.data._
+import play.api.data.Forms._
 
 object MsTableController extends Controller {
 
-  def index = Action {
-    Ok(<p>Hello World!</p>).as(HTML)
+  val msTableForm = Form(
+    "physicalTableName" -> nonEmptyText
+  )
+
+  def ms_tables = Action {
+    Ok(views.html.ms_tables(MsTable.list, msTableForm))
   }
 
-  def test1 = Action {
-    BadRequest("Bad Request!")
-  }
-
-  def test2 = {
-    Redirect("http://www.google.com")
-  }
-
-  def hello(name: String) = Action { request =>
-    val params: Map[String, Seq[String]] = request.queryString
-    val name = params("name").head
-    println(name)
-
-    Ok(<h1>Hello {name}!</h1>).as(HTML)
+  def new_ms_tables =  Action { implicit request =>
+    msTableForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.ms_tables(MsTable.list, errors)),
+      physicalTableName => {
+        MsTable.create(physicalTableName)
+        Redirect(routes.MsTableController.ms_tables)
+      }
+    )
   }
 
 }
