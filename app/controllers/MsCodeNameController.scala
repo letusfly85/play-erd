0a2ad3dfb8e24bee7b33d23361c99b3607c988c1
letusfly85 <letusfly95@gmail.com>
2013-09-16 10:10:14 +0900

    add ms_code_names modules

--- app/controllers/MsCodeNameController.scala
+++ app/controllers/MsCodeNameController.scala
@@ -1,5 +1,14 @@
 package controllers
 
-object MsCodeNameController {
+import play.api.mvc.{Action, Controller}
+import play.api.data._
+import play.api.data.Forms._
+import models.MsCodeName
+
+object MsCodeNameController extends Controller{
+
+  def ms_code_names = Action {
+    Ok(views.html.ms_code_names(MsCodeName.list))
+  }
 
 }
--- app/models/MsCodeName.scala
+++ app/models/MsCodeName.scala
@@ -1,7 +1,31 @@
 package models
 
-case class MsCodeName()
+import play.api.db._
+import play.api.Play.current
+
+import anorm.SqlParser._
+import anorm._
+import java.sql.Connection
+import anorm.~
+
+case class MsCodeName(codeId: String, logicalCodeName: String, physicalCodeName: String, logicalTableName: String, physicalTableName: String)
 
 object MsCodeName {
 
+  val ms_code_name = {
+    get[String]("PK_CODE_ID") ~
+    get[Option[String]]("LOGICAL_CODE_NAME") ~
+    get[Option[String]]("PHYSICAL_CODE_NAME") ~
+    get[String]("LOGICAL_TABLE_NAME") ~
+    get[String]("PHYSICAL_TABLE_NAME") map  {
+      case codeId ~ logicalCodeName ~ physicalCodeName ~ logicalTableName ~ physicalTableName =>
+        MsCodeName(codeId, logicalCodeName.getOrElse(""), physicalCodeName.getOrElse(""), logicalTableName, physicalTableName)
+    }
+  }
+
+  def list: List[MsCodeName] = {
+    DB.withConnection { implicit conn: Connection =>
+      SQL("SELECT * FROM MS_CODE_NAMES").as(ms_code_name *)
+    }
+  }
 }
